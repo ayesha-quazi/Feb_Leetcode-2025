24d58c43debac20ff2f6e150ead5ec245145c18b
Ayesha Quazi <aayeshaquazi04@gmail.com>
2025-02-02 22:36:14 +0530

    1 Feb

--- 1feb.cpp
+++ 1feb.cpp
@@ -1,3 +1,16 @@
 // Q: Special Array I
 
-!
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0; i <= n-2; i++) {
+            if(nums[i]%2 == nums[i+1]%2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
