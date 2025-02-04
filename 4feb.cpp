87ec1e3946c317b9b2da6673949127e5bbcbc175
Ayesha Quazi <aayeshaquazi04@gmail.com>
2025-02-04 08:58:07 +0530

    4 feb

---
+++ 4feb.cpp
@@ -0,0 +1,24 @@
+// Q: Maximum Ascending Subarray Sum
+
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxSum = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            int sum = nums[i];
+            int j = i+1;
+            while(j < n && nums[j] > nums[j-1]) {
+                sum += nums[j];
+                j++;
+            }
+
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+};
